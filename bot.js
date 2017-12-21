b4a3446931d43ebea98cec304bf7d7c0ec78a78f
aeris <aerisdoescodes@gmail.com>
2017-12-21 16:55:37 +0800

    Plox cat

---
+++ bot.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+
+//Defined it as client
+const client = new Discord.Client();
+
+const config = require('./config.json');
+
+//Insert bot prefix
+const prefix = '=='
+
+//Install snekfetch (npm install snekfetch)
+const {get} = require("snekfetch"); 
+
+client.on('ready', () =>{
+	console.log('I am ready!')
+});
+
+
+client.on('message', message => {
+	if (message.content.startsWith(prefix + 'cat')) {
+		try {
+			get('https://random.cat/meow').then(response => {
+				message.channel.send({files: [{attachment: response.body.file, name: `cat.${response.body.file.split('.')[2]}`}]});
+			})
+		} catch (e) {
+			console.log(e);
+		}
+	};
+});
+
+client.login(config.token);
