d691fa55ebec739d8a3fc18f8278655e95a8a5e8
aeris <aerisdoescodes@gmail.com>
2018-01-31 17:26:43 +0800

    Random Cat picture in embed file

--- bot.js
+++ bot.js
@@ -11,21 +11,38 @@ const prefix = '=='
 //Install snekfetch (npm install snekfetch)
 const {get} = require("snekfetch"); 
 
+//Embed
+const embed = new Discord.RichEmbed();
+
 client.on('ready', () =>{
 	console.log('I am ready!')
 });
 
 
 client.on('message', message => {
+
+	//Random Cat in Embed file
+	if (message.content.startsWith(prefix + 'embed-cat')) {
+		try {
+			get('https://random.cat/meow').then(response => {
+				embed.setImage(response.body.file);
+				message.channel.send({embed});
+			});
+		} catch (error) {
+			return message.channel.send(error.stack);
+		}
+	};
+
 	if (message.content.startsWith(prefix + 'cat')) {
 		try {
 			get('https://random.cat/meow').then(response => {
 				message.channel.send({files: [{attachment: response.body.file, name: `cat.${response.body.file.split('.')[2]}`}]});
-			})
+			});
 		} catch (e) {
-			console.log(e);
+			return message.channel.send(e.stack);
 		}
 	};
+
 });
 
 //Login to discord
